0dc6f348522938c2a14dd8129661c62d668fd07b
Jaws <52831164+Jaws576@users.noreply.github.com>
2024-04-20 21:47:34 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ set -u;
 ##
 
 echo -e '\n\033[1m[Build Image]\033[0m'
-docker build . -f linux.Dockerfile --rm -t lacledeslan/gamesvr-tf2classic:latest --no-cache --pull --build-arg BUILDNODE="$(cat /proc/sys/kernel/hostname)";
+docker build . -f linux.Dockerfile --rm -t jaws576/tf2c-server:latest --no-cache --pull --build-arg BUILDNODE="$(cat /proc/sys/kernel/hostname)";
 
 
 echo -e '\n\033[1m[Running Self-Checks]\033[0m'
-docker run -it --rm lacledeslan/gamesvr-tf2classic:latest ./ll-tests/gamesvr-tf2classic.sh
+docker run -it --rm jaws576/tf2c-server:latest ./ll-tests/gamesvr-tf2classic.sh
 
 
 echo -e '\n\033[1m[Pushing to Docker Hub]\033[0m'
-echo "> push lacledeslan/gamesvr-tf2:base"
-docker push lacledeslan/gamesvr-tf2classic:latest
+echo "> push jaws576/tf2c-server:base"
+docker push jaws576/tf2c-server:latest
