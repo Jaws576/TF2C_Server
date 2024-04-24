b42efc3dc7b25f2c3f1046b2e54bfe70e493174e
Jaws <52831164+Jaws576@users.noreply.github.com>
2024-04-24 02:39:28 +0100

    Update native_build.sh

--- native_build.sh
+++ native_build.sh
@@ -4,10 +4,10 @@ apt-get update;
 apt-get install libxcb-xinerama0 -y;
 apt-get install screen -y;
 
-
+add-apt-repository multiverse
 dpkg --add-architecture i386;
 apt-get update;
-apt-get install -y ca-certificates lib32gcc-s1 libtinfo5:i386 libcurl4-gnutls-dev:i386 libstdc++6 libstdc++6:i386 locales locales-all tmux zlib1g:i386;
+apt-get install -y ca-certificates lib32z1 libncurses5:i386 libbz2-1.0:i386 lib32gcc-s1 lib32stdc++6 libtinfo5:i386 libcurl3-gnutls:i386 libsdl2-2.0-0:i386 libcurl4-gnutls-dev libcurl4-gnutls-dev:i386
 apt-get clean;
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment;
 rm -rf /tmp/* /var/lib/apt/lists/* /var/tmp/*;
