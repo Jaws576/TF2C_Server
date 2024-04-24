fe87ab756112e43857413f2f18b25c746f928983
Jaws <52831164+Jaws576@users.noreply.github.com>
2024-04-24 03:24:07 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -7,23 +7,19 @@ NAME=SRCDS
 case $1 in
       start)
           screen -d -m -S $NAME $DAEMON $PARAMS
-          fi
           ;;
       stop)
           screen -S $NAME -X quit
-          fi
           ;;
       restart)
           screen -S $NAME -X quit
           screen -d -m -S $NAME $DAEMON $PARAMS
-          fi
           ;;
       update)
           screen -S $NAME -X quit
           /app/updater/TF2CDownloaderLinux --update /app/server/
           rm -rf /var/tmp/*;
           screen -d -m -S $NAME $DAEMON $PARAMS
-          fi
           ;;
 esac
 
