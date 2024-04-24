7e4e44a67be23f48ea16e876c10fd30d00d5b40d
Jaws <52831164+Jaws576@users.noreply.github.com>
2024-04-24 13:52:40 +0100

    Update native_build.sh

--- native_build.sh
+++ native_build.sh
@@ -24,7 +24,6 @@ mkdir -p /app/server/ll-tests;
 chmod +x /app/updater/TF2CDownloaderLinux
 /app/updater/TF2CDownloaderLinux --install /app/server/;
 rm -rf /var/tmp/*;
-apt-get purge steamcmd -y
 
 cp -r ./dist/linux/* /app/server/
 
